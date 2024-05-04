ebb9fc9744124752a2ff7d25583ed72fb0cf9425
nihar <niharchopra59@gmail.com>
2024-05-04 18:20:45 +0530

    Merged 2 datasets

--- movie_recommender.py
+++ movie_recommender.py
@@ -4,3 +4,4 @@ import pandas as pd
 movies = pd.read_csv('movie_recommender_system/tmdb_5000_movies.csv')
 credits = pd.read_csv('movie_recommender_system/tmdb_5000_credits.csv')
 
+movies = movies.merge(credits, on='title')
