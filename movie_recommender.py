1d3c9b83f322ad33c99324c0c351b195ca2d9135
nihar <niharchopra59@gmail.com>
2024-05-04 19:24:28 +0530

    Extracted keywords from genres & keywords columns

--- movie_recommender.py
+++ movie_recommender.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import ast
 
 movies = pd.read_csv('movie_recommender_system/tmdb_5000_movies.csv')
 credits = pd.read_csv('movie_recommender_system/tmdb_5000_credits.csv')
@@ -7,3 +8,16 @@ credits = pd.read_csv('movie_recommender_system/tmdb_5000_credits.csv')
 movies = movies.merge(credits, on='title')
 
 movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+
+movies.dropna(inplace=True)  # Removing missing data
+
+
+def convert(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        L.append(i['name'])
+    return L
+
+
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
