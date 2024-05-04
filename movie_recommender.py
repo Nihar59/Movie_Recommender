57dd0b657306478cc39d87692dd18863dbf8844f
nihar <niharchopra59@gmail.com>
2024-05-04 18:28:16 +0530

    Removed data columns that don't go with our need of content based recommender system. This data won't help in making tags for movies

--- movie_recommender.py
+++ movie_recommender.py
@@ -5,3 +5,5 @@ movies = pd.read_csv('movie_recommender_system/tmdb_5000_movies.csv')
 credits = pd.read_csv('movie_recommender_system/tmdb_5000_credits.csv')
 
 movies = movies.merge(credits, on='title')
+
+movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
