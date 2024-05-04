79b26721b39dbf860a4dbd220349700b1934b2c5
nihar <niharchopra59@gmail.com>
2024-05-04 19:49:48 +0530

    Applying Stemming on tags data using PorterStemmer from nltk library

--- movie_recommender.py
+++ movie_recommender.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import ast
+import nltk
 
 movies = pd.read_csv('movie_recommender_system/tmdb_5000_movies.csv')
 credits = pd.read_csv('movie_recommender_system/tmdb_5000_credits.csv')
@@ -49,14 +50,13 @@ def fetch_director(obj):
 
 movies['crew'] = movies['crew'].apply(fetch_director)
 
-movies['overview'] = movies['overview'].apply(lambda x:x.split())       # Converting Overview from string to list
-
+movies['overview'] = movies['overview'].apply(lambda x: x.split())  # Converting Overview from string to list
 
 # Removing Spaces from between the words which should be a single tag. For eg: "Sam Worthington" -> "SamWorthington" & "Sam Mendes" -> "SamMendes"
-movies['genres'] = movies['genres'].apply(lambda x:[i.replace(" ", "") for i in x])
-movies['keywords'] = movies['keywords'].apply(lambda x:[i.replace(" ", "") for i in x])
-movies['cast'] = movies['cast'].apply(lambda x:[i.replace(" ", "") for i in x])
-movies['crew'] = movies['crew'].apply(lambda x:[i.replace(" ", "") for i in x])
+movies['genres'] = movies['genres'].apply(lambda x: [i.replace(" ", "") for i in x])
+movies['keywords'] = movies['keywords'].apply(lambda x: [i.replace(" ", "") for i in x])
+movies['cast'] = movies['cast'].apply(lambda x: [i.replace(" ", "") for i in x])
+movies['crew'] = movies['crew'].apply(lambda x: [i.replace(" ", "") for i in x])
 
 # New Column combining overview, genres, keywords, cast and crew
 movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
@@ -65,9 +65,21 @@ movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + mo
 new_df = movies[['movie_id', 'title', 'tags']]
 
 # Converting list of words in tags column to string format
-new_df['tags'] = new_df['tags'].apply(lambda x:" ".join(x))
+new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x))
 
 # Transforming all the data in tags column to lowercase
-new_df['tags'] = new_df['tags'].apply(lambda x:x.lower())
+new_df['tags'] = new_df['tags'].apply(lambda x: x.lower())
 
+
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+
+def stem(text):
+    txt = []
+    for i in text.split():
+        txt.append(ps.stem(i))
+
+    return " ".join(txt)
+
+new_df['tags'] = new_df['tags'].apply(stem)
 
